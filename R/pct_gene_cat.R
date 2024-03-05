ce26d4099e9c341ffc5dea04fdf1823dd4c83969
Sneha Korlakunta <Sneha.Korlakunta@UTSouthwestern.edu>
2024-03-05 03:51:33 -0600

    Update function documentation and imports

--- R/pct_gene_cat.R
+++ R/pct_gene_cat.R
@@ -4,6 +4,8 @@
 #'    pattern and a set threshold for max percent
 #'
 #' @param gene_cat String category for selected gene subset
+#' @param dataset_name Name of current dataset
+#' @param counts_file Unprocessed barcode-feature-matrix file
 #' @param pattern String for grep-ing genes
 #' @param max_pct Threshold for max-cutoff for genes
 #' @param print_genes Choose whether to print applicable genes, default is FALSE
@@ -11,9 +13,12 @@
 #' @return A sorted scatterplot with a set max threshold
 #'
 #' @export
-pct_gene_cat <- function(gene_cat, pattern, max_pct, print_genes=FALSE) {
+#'
+pct_gene_cat <- function(gene_cat, dataset_name, counts_file, pattern, max_pct, print_genes=FALSE) {
   cat_genes <- grep(pattern, rownames(counts_file), ignore.case=T, value=T)
   if (print_genes) print(cat_genes)
+
+  counts_per_cell <- Matrix::colSums(counts_file)
 
   # compute pct genes in category
   cat_gene_read_counts = Matrix::colSums(counts_file[cat_genes,])
